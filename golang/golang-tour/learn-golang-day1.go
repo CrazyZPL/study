498529eefbab2fe42e1ec13d8856468274dad165
crazyzpl <crazyzplzpl@163.com>
2022-05-26 15:02:05 +0800

    learned golang type assertons

--- golang/golang-tour/learn-golang-day1.go
+++ golang/golang-tour/learn-golang-day1.go
@@ -247,17 +247,44 @@ import (
 // }
 
 // empty interface
-func main() {
-	var i interface{}
-	describe(i)
+// func main() {
+// 	var i interface{}
+// 	describe(i)
 
-	i = 42
-	describe(i)
+// 	i = 42
+// 	describe(i)
 
-	i = "hello"
-	describe(i)
+// 	i = "hello"
+// 	describe(i)
+// }
+// func describe(i interface{}) {
+// 	fmt.Printf("(%v, %T)\n", i, i)
+// }
+
+// type assertions
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
 }
+func main() {
+	var i interface{} = "hello"
 
-func describe(i interface{}) {
-	fmt.Printf("(%v, %T)\n", i, i)
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	do(21)
+	do("hello")
+	do(true)
 }
