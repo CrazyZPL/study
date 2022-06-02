2434041299abb345bb512d6dd66df231fae6f441
crazyzpl <crazyzplzpl@163.com>
2022-06-02 19:25:15 +0800

    learn go cli

--- golang/golang-tour/learn-golang-day1.go
+++ golang/golang-tour/learn-golang-day1.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	// "math"
-	// "strings"
+	"io"
+	"strings"
 )
 
 // for .. range...
@@ -262,29 +263,40 @@ import (
 // }
 
 // type assertions
-func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
-}
+// func do(i interface{}) {
+// 	switch v := i.(type) {
+// 	case int:
+// 		fmt.Printf("Twice %v is %v\n", v, v*2)
+// 	case string:
+// 		fmt.Printf("%q is %v bytes long\n", v, len(v))
+// 	default:
+// 		fmt.Printf("I don't know about type %T!\n", v)
+// 	}
+// }
+// func main() {
+// 	var i interface{} = "hello"
+// 	s := i.(string)
+// 	fmt.Println(s)
+// 	s, ok := i.(string)
+// 	fmt.Println(s, ok)
+// 	f, ok := i.(float64)
+// 	fmt.Println(f, ok)
+// 	do(21)
+// 	do("hello")
+// 	do(true)
+// }
+
+// readers
 func main() {
-	var i interface{} = "hello"
+	r := strings.NewReader("Hello, Reader!")
 
-	s := i.(string)
-	fmt.Println(s)
-
-	s, ok := i.(string)
-	fmt.Println(s, ok)
-
-	f, ok := i.(float64)
-	fmt.Println(f, ok)
-
-	do(21)
-	do("hello")
-	do(true)
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
 }
